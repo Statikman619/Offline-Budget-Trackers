bea87250937256a9176227e46bdc64658f262c49
Statikman619 <martinez.michael02@gmail.com>
2021-07-24 12:52:05 -0700

    almost finished the service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,5 +38,8 @@ self.addEventListener("fetch", (event) => {
       })
     );
   }
-  event.respondWith(fetch.(event.request)    
+  event.respondWith(fetch.(event.request)
+  .catch(() => {
+    return caches.match(event.request).then(function(response))
+  })
 });
