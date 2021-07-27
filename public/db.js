a3c6426f4ad47ad4c7b6e3d41dbaad8d55d39c35
Statikman619 <martinez.michael02@gmail.com>
2021-07-26 17:29:38 -0700

    fixed most of my bugs

--- public/db.js
+++ public/db.js
@@ -40,3 +40,31 @@ function checkDatabase() {
   const store = transaction.objectStore("trackerStore");
   // get all records from store and set to a variable
   const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your trackerStore db
+          const transaction = db.transaction(["trackerStore"], "readwrite");
+
+          // access your trackerStore object store
+          const store = transaction.objectStore("trackerStore");
+
+          // clear all items in your store
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
